ad233c5e7b0c1a6b7d914743ac6531b2bf227add
myideagithub2021 <15810140695@126.com>
2021-02-09 22:44:00 +0800

    第一次进行数据提交

---
+++ Model01/src/com/msb/Hello.java
@@ -0,0 +1,11 @@
+package com.msb;
+
+/**
+ * @author shkstart
+ * @create 2021-02-09 22:39
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
